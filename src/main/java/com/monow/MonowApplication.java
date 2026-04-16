84636cee62159fe22c7c6691fb20c2636ebb5608
mini0-0 <nalala8200@naver.com>
2026-04-16 23:50:55 +0900

    chore: 프로젝트 초기 생성
    - Java 21
    - Spring Boot 3.5.14

---
+++ src/main/java/com/monow/MonowApplication.java
@@ -0,0 +1,13 @@
+package com.monow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monow/MonowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
