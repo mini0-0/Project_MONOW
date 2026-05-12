204ab8006e2abaca23a356f4439119e68b9fb813
mini0-0 <nalala8200@naver.com>
2026-05-13 00:03:01 +0900

    feat: 로그인 service, controller 테스트 추가
    - AuthServiceTest에 로그인 성공 및 실패 케이스 추가
    - 가입되지 않은 이메일과 비밀번호 불일치 시  -> LOGIN_FAIL 예외 발생 검증
    - AuthControllerTest에 로그인 성공 응답 검증 추가
    - 로그인 실패 시 LOGIN_FAIL 검증
    - 이메일 누락, 비밀번호 누락, 이메일 형식 오류에 대한 Validation 응답 검증

--- api/src/test/java/com/monow/api/auth/controller/AuthControllerTest.java
+++ api/src/test/java/com/monow/api/auth/controller/AuthControllerTest.java
@@ -1,6 +1,7 @@
 package com.monow.api.auth.controller;
 
 
+import com.monow.domain.auth.dto.command.LoginCommand;
 import com.monow.domain.auth.dto.command.SignUpCommand;
 import com.monow.domain.auth.dto.result.AuthResult;
 import com.monow.domain.auth.service.AuthService;
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +23,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.never;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -51,7 +54,6 @@ public class AuthControllerTest {
         AuthResult authResult = new AuthResult(
                 1L,
                 "test@test.com",
-                "1234",
                 "홍길동",
                 "워렌버핏",
                 UserRole.USER,
@@ -65,8 +67,7 @@ public class AuthControllerTest {
         mockMvc.perform(post("/api/v1/auth/signup")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestBody))
-                .andDo(print())
-//                .andExpect(status().isOk())
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data.userId").value(1L))
                 .andExpect(jsonPath("$.data.email").value("test@test.com"))
@@ -272,13 +273,173 @@ public class AuthControllerTest {
 
     }
 
+    @Test
+    @DisplayName("로그인 API 성공")
+    void loginSuccess() throws Exception{
+        // Given
+        String requestBody = """
+                {
+                    "email": "test@test.com",
+                    "password": "1234"
+                    
+                }
+                """;
+
+        AuthResult authResult = new AuthResult(
+                1L,
+                "test@test.com",
+                "홍길동",
+                "워렌버핏",
+                UserRole.USER,
+                UserStatus.ACTIVE);
+
+        given(authService.login(any(LoginCommand.class)))
+                .willReturn(authResult);
+
+        // When & Then
+        mockMvc.perform(post("/api/v1/auth/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+//                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.userId").value(1L))
+                .andExpect(jsonPath("$.data.email").value("test@test.com"))
+                .andExpect(jsonPath("$.data.name").value("홍길동"))
+                .andExpect(jsonPath("$.data.nickname").value("워렌버핏"))
+                .andExpect(jsonPath("$.data.userRole").value("USER"))
+                .andExpect(jsonPath("$.data.status").value("ACTIVE"))
+                ;
 
 
+    }
+
+    @Test
+    @DisplayName("로그인 API 실패 - 회원가입 안한 계정")
+    void loginFail_notSignUp() throws Exception {
+        String requestBody = """
+                {
+                    "email": "test@test.com",
+                    "password": "1234"
+                    
+                }
+                """;
+
+        given(authService.login(any(LoginCommand.class)))
+                .willThrow(new BusinessException(ErrorCode.LOGIN_FAIL));
+
+        // When & Then
+        mockMvc.perform(post("/api/v1/auth/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+//                    .andDo(print())
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("LOGIN_FAIL"))
+                .andExpect(jsonPath("$.error.message").value("이메일 또는 비밀번호가 일치하지 않습니다."));
+
+        verify(authService).login(any(LoginCommand.class));
+    }
+
+    @Test
+    @DisplayName("로그인 API 실패 - 비밀번호 불일치")
+    void loginFail_invalidPassword() throws Exception{
+        String requestBody = """
+                {
+                    "email": "test@test.com",
+                    "password": "0000"
+                    
+                }
+                """;
 
 
+        given(authService.login(any(LoginCommand.class)))
+                .willThrow(new BusinessException(ErrorCode.LOGIN_FAIL));
 
-    // 로그인 api 성공
+        // When & Then
+        mockMvc.perform(post("/api/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+//                        .andDo(print())
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("LOGIN_FAIL"))
+                .andExpect(jsonPath("$.error.message").value("이메일 또는 비밀번호가 일치하지 않습니다."));
 
-    // 로그인 api 실패
+        verify(authService).login(any(LoginCommand.class));
+
+    }
+
+    @Test
+    @DisplayName("로그인 API 실패 - 이메일 누락")
+    void loginApiFail_emailBlank() throws Exception {
+        // Given
+        String requestBody = """
+            {
+                "email": "",
+                "password": "1234"
+            }
+            """;
+
+        // When & Then
+        mockMvc.perform(post("/api/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("INVALID_REQUEST"))
+                .andExpect(jsonPath("$.error.message").value("이메일은 필수 입력값입니다."));
+
+        verify(authService, never()).login(any(LoginCommand.class));
+    }
+
+    @Test
+    @DisplayName("로그인 API 실패 - 비밀번호 누락")
+    void loginApiFail_passwordBlank() throws Exception {
+        // Given
+        String requestBody = """
+            {
+                "email": "test@test.com",
+                "password": ""
+            }
+            """;
+
+        // When & Then
+        mockMvc.perform(post("/api/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("INVALID_REQUEST"))
+                .andExpect(jsonPath("$.error.message").value("비밀번호는 필수 입력값입니다."));
+
+        verify(authService, never()).login(any(LoginCommand.class));
+    }
+
+    @Test
+    @DisplayName("로그인 API 실패 - 이메일 형식 오류")
+    void loginApiFail_invalidEmail() throws Exception {
+        // Given
+        String requestBody = """
+            {
+                "email": "test-email",
+                "password": "1234"
+            }
+            """;
+
+        // When & Then
+        mockMvc.perform(post("/api/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("INVALID_REQUEST"))
+                .andExpect(jsonPath("$.error.message").value("올바른 이메일 형식이 아닙니다."));
+
+        verify(authService, never()).login(any(LoginCommand.class));
+    }
 
 }
