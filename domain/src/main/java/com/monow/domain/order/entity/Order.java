f62a6a02ed03422be164cd0caf2248bac714ba68
mini0-0 <nalala8200@naver.com>
2026-08-07 02:04:47 +0900

    feat: 주식 주문 관리 기능 추가
    - 주식 주문 정보를 저장하는 Order 추가
    - 매수와 매도 주문을 구분하는 OrderType 추가
    - 주문 상태 처리(생성, 체결, 취소, 실패)를 구분하는 OrderStatus 추가
    - 주문 정보 저장과 조회를 위한 OrderRepository 추가

---
+++ domain/src/main/java/com/monow/domain/order/entity/Order.java
@@ -0,0 +1,113 @@
+package com.monow.domain.order.entity;
+
+import com.monow.domain.account.entity.Account;
+import com.monow.domain.stock.entity.Stock;
+import com.monow.domain.user.entity.User;
+import com.monow.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_id", nullable = false)
+    private Stock  stock;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "order_type", nullable = false, length = 20)
+    private OrderType orderType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "order_status", nullable = false, length = 50)
+    private OrderStatus orderStatus;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "order_price", nullable = false, precision = 18, scale = 2)
+    private BigDecimal orderPrice;
+
+    @Column(name = "total_amount", nullable = false, precision = 18, scale = 2)
+    private BigDecimal totalAmount;
+
+    @Column(name = "fail_reason", length = 100)
+    private String failReason;
+
+    @Column(name = "requested_at")
+    private LocalDateTime requestedAt;
+
+    @Column(name = "executed_at")
+    private LocalDateTime executedAt;
+
+    @Column(name = "canceled_at")
+    private LocalDateTime canceledAt;
+
+    private Order(
+            User user,
+            Account account,
+            Stock stock,
+            OrderType orderType,
+            OrderStatus orderStatus,
+            Integer quantity,
+            BigDecimal orderPrice,
+            BigDecimal totalAmount,
+            LocalDateTime requestedAt,
+            LocalDateTime executedAt
+    ) {
+        this.user = user;
+        this.account = account;
+        this.stock = stock;
+        this.orderType = orderType;
+        this.orderStatus = orderStatus;
+        this.quantity = quantity;
+        this.orderPrice = orderPrice;
+        this.totalAmount = totalAmount;
+        this.requestedAt = requestedAt;
+        this.executedAt = executedAt;
+    }
+
+    public static Order createOrder(
+            User user,
+            Account account,
+            Stock stock,
+            Integer quantity,
+            BigDecimal orderPrice,
+            BigDecimal totalAmount,
+            LocalDateTime requestedAt,
+            LocalDateTime executedAt
+    ) {
+        return new Order(
+                user,
+                account,
+                stock,
+                OrderType.BUY,
+                OrderStatus.EXECUTED,
+                quantity,
+                orderPrice,
+                totalAmount,
+                requestedAt,
+                executedAt
+        );
+    }
+}
