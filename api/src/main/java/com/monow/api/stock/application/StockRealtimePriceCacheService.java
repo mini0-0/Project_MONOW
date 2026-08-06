60099d9f102998d90b414206973afcd687f912b5
mini0-0 <nalala8200@naver.com>
2026-08-07 01:44:44 +0900

    feat: Redis에서 최신 실시간 주가 조회 기능 추가
    - stockCode와 marketType를 기준으로 Redis에 저장된 최신 실시간 주가 조회
    - 조회한 캐시 데이터를 실시간 주가 응답 형태로 반환
    - 저장된 실시간 주가가 없는 경우를 처리하도록 findLatestPrice 구현

--- api/src/main/java/com/monow/api/stock/application/StockRealtimePriceCacheService.java
+++ api/src/main/java/com/monow/api/stock/application/StockRealtimePriceCacheService.java
@@ -3,6 +3,8 @@ package com.monow.api.stock.application;
 import com.monow.api.external.kis.type.CurrentPriceMarketType;
 import com.monow.api.stock.dto.response.RealtimeStockPriceResponse;
 import com.monow.api.stock.dto.response.StockCurrentPriceResponse;
+import com.monow.global.error.exception.BusinessException;
+import com.monow.global.error.model.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -30,5 +32,36 @@ public class StockRealtimePriceCacheService {
                 redisKey,
                 response.currentPrice()
         );
+    }
+
+    public RealtimeStockPriceResponse findLatestPrice(
+            CurrentPriceMarketType marketType,
+            String stockCode
+    ) {
+        String redisKey = REDIS_KEY_PREFIX + marketType.name() + ":" + stockCode;
+
+        Object cachedValue = redisTemplate.opsForValue().get(redisKey);
+
+        if (cachedValue == null) {
+            log.warn(
+                    "Redis 실시간 주가 조회 실패 - redisKey={}",
+                    redisKey
+            );
+            throw new BusinessException(ErrorCode.REALTIME_STOCK_PRICE_NOT_FOUND);
+        }
+
+        if (!(cachedValue instanceof RealtimeStockPriceResponse)) {
+            log.warn(
+                    "Redis 실시간 주가 타입 오류 - redisKey={}, actualType={}",
+                    redisKey,
+                    cachedValue.getClass().getSimpleName()
+            );
+            throw  new BusinessException(ErrorCode.REALTIME_PRICE_INVALID_RESPONSE);
+        }
+
+        RealtimeStockPriceResponse response = (RealtimeStockPriceResponse) cachedValue;
+
+        return response;
+
     }
 }
