7076620a7487a2d0d5a1d613bda1e0102801b309
mini0-0 <nalala8200@naver.com>
2026-09-17 14:09:42 +0900

    feat: 일별 시세 Batch Job 구성
    - Stock 목록을 조회하는 Reader 구성
    - Reader, Processor, Writer 기반 Step 구성
    - 일별 시세 처리를 위한 Batch Job 등록
    - Step 실행 단위로 Reader가 생성되도록 StepScope 적용

---
+++ batch/src/main/java/com/monow/batch/stock/dailyprice/job/DailyStockPriceJobConfig.java
@@ -0,0 +1,66 @@
+package com.monow.batch.stock.dailyprice.job;
+
+import com.monow.batch.stock.dailyprice.processor.DailyStockPriceProcessor;
+import com.monow.batch.stock.dailyprice.writer.DailyStockPriceWriter;
+import com.monow.domain.stock.entity.Stock;
+import com.monow.domain.stock.entity.StockPriceDaily;
+import com.monow.domain.stock.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class DailyStockPriceJobConfig {
+
+    private final JobRepository jobRepository;
+
+    private final PlatformTransactionManager transactionManager;
+
+    private final StockRepository stockRepository;
+
+    private final DailyStockPriceProcessor dailyStockPriceProcessor;
+
+    private final DailyStockPriceWriter dailyStockPriceWriter;
+
+    @Bean
+    public Job dailyStockPriceJob(Step dailyStockPriceStep) {
+
+        return new JobBuilder("dailyStockPriceJob", jobRepository)
+                .start(dailyStockPriceStep)
+                .build();
+
+    }
+
+    @Bean
+    public Step dailyStockPriceStep(ItemReader<Stock> dailyStockPriceReader) {
+
+        return new StepBuilder("dailyStockPriceStep", jobRepository)
+                .<Stock, List<StockPriceDaily>>chunk(10, transactionManager)
+                .reader(dailyStockPriceReader)
+                .processor(dailyStockPriceProcessor)
+                .writer(dailyStockPriceWriter)
+                .build();
+
+
+    }
+
+
+    @Bean
+    @StepScope
+    public ItemReader<Stock> dailyStockPriceReader() {
+        return new ListItemReader<>(stockRepository.findAll());
+    }
+
+}
