ca3d13e5afac240981b8cc679c72b81da5c02a89
mini0-0 <nalala8200@naver.com>
2026-09-17 14:19:08 +0900

    feat: 일별 시세 Batch 스케줄링 추가
    - 평일 장 마감 이후 일별 시세 Batch Job 자동 실행
    - 실행마다 JobParameter를 생성하여 Batch 실행 구분
    - Batch 스케줄링 활성화
    - 애플리케이션 시작 시 Job 자동 실행 방지

---
+++ batch/src/main/java/com/monow/batch/stock/dailyprice/scheduler/DailyStockPriceScheduler.java
@@ -0,0 +1,28 @@
+package com.monow.batch.stock.dailyprice.scheduler;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DailyStockPriceScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final Job dailyStockPriceJob;
+
+    @Scheduled(cron = "0 10 16 * * MON-FRI", zone = "Asia/Seoul")
+    public void runDailyStockPriceJob() throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("timestamp", System.currentTimeMillis())
+                .toJobParameters();
+
+        jobLauncher.run(dailyStockPriceJob, jobParameters);
+    }
+
+}
