f5c51353b4ef006639b9deef34b9fce22bd7785d
mini0-0 <nalala8200@naver.com>
2026-09-25 16:58:09 +0900

    chore: BatchApplication, yaml 파일 수정
    - kis.yaml 파일 위치 변경
    - application.yaml 오타 수정
    - baseentity 설정 추가

--- batch/src/main/java/com/monow/batch/BatchApplication.java
+++ batch/src/main/java/com/monow/batch/BatchApplication.java
@@ -3,11 +3,13 @@ package com.monow.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableRetry
+@EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = "com.monow")
 @EntityScan(basePackages = "com.monow.domain")
 @EnableJpaRepositories(basePackages = "com.monow.domain")
